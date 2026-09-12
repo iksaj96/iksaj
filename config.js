4b509cdc8ea87a25e4ef42d8f96f28505af38e60
iksaj96 <iksaj96@gmail.com>
2026-09-12 17:10:41 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: "https://bzyxjdtvkhwzkdxjirjq.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6eXhqZHR2a2h3emtkeGppcmpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTU3ODEsImV4cCI6MjEwNDczMTc4MX0.AmBG_tssNSVrd0W-7A_w30x4UpjtN1KOu-lISLEoLwo" // Ganti dengan Anon Key Supabase Anda
+};
